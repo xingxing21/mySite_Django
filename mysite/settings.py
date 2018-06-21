a6c7ea238c26709d46ad443782eab2811f95b6b9
osk1985 <osk.samhong@gmail.com>
2018-06-22 00:48:33 +0800

    Initial Version 0.1

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,5 +136,6 @@ USE_TZ = True
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
